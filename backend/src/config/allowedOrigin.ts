1230a7ec1f21f8d17cd346d14adaa14e26c4f802
paramsgit <param11650@gmail.com>
2026-03-20 00:37:46 +0530

    chore: backend api updated

--- backend/src/config/allowedOrigin.ts
+++ backend/src/config/allowedOrigin.ts
@@ -1,4 +1,5 @@
 const allowedOrigins = () => {
+  console.log(process.env.ALLOWED_ORIGINS);
   return JSON.parse(process.env.ALLOWED_ORIGINS || "[]");
 };
 export default allowedOrigins;
--- frontend/webapp/src/api/api.ts
+++ frontend/webapp/src/api/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+console.log("VITE_API_URL", import.meta.env.VITE_API_URL);
+
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`, // change to your backend URL
+  baseURL: `http://api.files.params.online/api`, // change to your backend URL
   withCredentials: true, // if you use cookies / auth
 });
