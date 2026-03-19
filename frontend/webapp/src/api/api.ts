6aa359242e4ef022e2d344a1f7c41f41165124e5
paramsgit <param11650@gmail.com>
2026-03-20 00:26:54 +0530

    update: frontend domain configured

--- frontend/webapp/src/api/api.ts
+++ frontend/webapp/src/api/api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-console.log(import.meta.env.VITE_API_URL);
-
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // change to your backend URL
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // change to your backend URL
   withCredentials: true, // if you use cookies / auth
 });
