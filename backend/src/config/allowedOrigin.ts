726bd75308a907de6c6fa8ef2540a0b434d77676
paramsgit <param11650@gmail.com>
2026-03-20 11:25:20 +0530

    update: cors

--- backend/src/config/allowedOrigin.ts
+++ backend/src/config/allowedOrigin.ts
@@ -1,5 +1,4 @@
 const allowedOrigins = () => {
-  console.log(process.env.ALLOWED_ORIGINS);
-  return JSON.parse(process.env.ALLOWED_ORIGINS || "[]");
+  return ["http://files.params.online", "https://files.params.online"];
 };
 export default allowedOrigins;
